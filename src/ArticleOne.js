99e7baf02b68eb18b5dba03cb9910ebcf2787b2d
Marcus Richard <msrichard18@gmail.com>
2017-02-24 21:40:31 -0500

    created Article One component

---
+++ src/ArticleOne.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class ArticleOne extends Component {
+  render() {
+    return (
+      <div>Article One
+      </div>
+    );
+  }
+}
+
+export default ArticleOne;
